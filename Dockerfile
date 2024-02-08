80fb3393e125e82d47a191542750dc381c577a00
Amal Paul <amalpaul@Amals-MacBook-Air.local>
2024-02-09 00:12:23 +0530

    Make Changes for plane db

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ LABEL org.opencontainers.image.source="https://github.com/Tacten/mysql-s3-backup
 LABEL org.opencontainers.image.url="https://github.com/Tacten/mysql-s3-backup"
 
 RUN apk upgrade --no-cache \
-  && apk add --no-cache mysql-client python3 py3-six py3-pip py3-setuptools libmagic git ca-certificates jq \
+  && apk add --no-cache postgresql-client python3 py3-six py3-pip py3-setuptools libmagic git ca-certificates jq \
   && git clone https://github.com/s3tools/s3cmd.git /tmp/s3cmd \
   && cd /tmp/s3cmd \
   && pip install python-dateutil python-magic --break-system-packages\
--- backup-script.sh
+++ backup-script.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 #Getting mysql user and password
-mkdir /s3/$SITE
-mkdir /s3/$SITE/database_backups
-cd /s3/$SITE
+mkdir -p /s3/${SITE}/uploads
+mkdir -p /s3/${SITE}/database_backups
+cd /s3/
 
-site_config_path="/sites/$SITE/site_config.json"
-mysql_user=$(jq -r '.db_name' $site_config_path)
-mysql_password=$(jq -r '.db_password' $site_config_path)
-
-cp -r /sites/$SITE /s3/$SITE/
-file="$(date +%F-%H)_${SERVER_NAME}_${SITE}_mysqldump.sql"
-mysqldump -h ${MYSQL_HOST} -u "$mysql_user" --password="$mysql_password" $mysql_user > database_backups/"$file"
+cp -r /uploads/ /s3/${SITE}/
+file="/s3/${SITE}/database_backups/$(date +%F-%H)_${SERVER_NAME}_${DB}_postgres.sql.gz"
+pg_dumpall -U plane | gzip > ${file}
 if [ $? -eq 0 ]; then
-    echo "mysqldump successfull\nUploading Now...."
+    echo "postgresql dump successfull\nUploading Now...."
     s3cmd put --recursive --ssl --access_key=${ACCESS_KEY} --secret_key=${SECRET_KEY} /s3/${SITE} s3://${BUCKET_NAME}/
     if [ $? -eq 0 ]; then
         wget http://healthchecks.tacten.link/ping/${HEALTHCHECK_SLUG} -T 10 -t 5 -O /dev/null
     fi
 fi
-rm "$file"
+rm -r /s3/${SITE}/*
